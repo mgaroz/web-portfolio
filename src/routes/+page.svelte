c406952f8ca50187dd3d112aa742adff2f560afb
Miguel Garoz <mgaroz@gmail.com>
2023-03-21 17:23:27 -0600

    added arrow icon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,42 @@
-<section class="px-20 h-[calc(100vh-128px)]">
+<section class="h-[calc(100vh-128px)] px-20">
 	<div
 		id="hero"
-		class="w-full relative z-20 overflow-hidden flex flex-col items-center justify-around h-full"
+		class="relative z-20 flex h-full w-full flex-col items-center justify-around overflow-hidden"
 	>
-		<div class="relative w-full inset-0 block mx-auto z-2">
-			<div class="pt-10 pb-40 w-full mx-auto relative text-left">
+		<div class="z-2 relative inset-0 mx-auto block w-full">
+			<div class="relative mx-auto w-full pt-10 pb-40 text-left">
 				<h1 class="text-hero-size leading-none">
-					<span class="block relative">MIGUEL</span>
-					<span class="inline-block relative left-96">GAROZ</span>
+					<span class="relative block">MIGUEL</span>
+					<span class="relative left-96 inline-block">GAROZ</span>
 				</h1>
 			</div>
 			<div class="absolute bottom-0 left-0 max-w-[16.25rem]" id="herosub-wrapper">
 				<h5 class="translate-y-[calc(-23vh+50%)] translate-x-2 uppercase leading-5">
-					<span class="text-sm relative block"
+					<span class="relative block text-sm"
 						>Front-end developer with an adaptive approach to problem solving.</span
 					>
 				</h5>
 			</div>
 		</div>
-		<div class="uppercase flex items-center justify-between w-full" id="hero-footer">
-			<div id="hero-footer-left" class="overflow-hidden">
-				<span class="inline-block text-sm slide-up cursor-pointer">Scroll to explore</span>
+		<div class="flex w-full items-center justify-between uppercase" id="hero-footer">
+			<div id="hero-footer-left" class="flex items-center justify-center gap-8 overflow-hidden">
+				<span class="slide-up inline-block cursor-pointer text-sm">Scroll to explore</span>
+				<div class="flex items-center justify-center">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width={2.5}
+						stroke="currentColor"
+						class="h-5 w-5"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M4.5 4.5l15 15m0 0V8.25m0 11.25H8.25"
+						/>
+					</svg>
+				</div>
 			</div>
 			<div id="hero-footer-right">
 				<span class="text-sm">Selected case studies (04)</span>
