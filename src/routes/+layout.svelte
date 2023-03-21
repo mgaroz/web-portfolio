a5d32c07b97dd85b35e30324776eb74e065834a9
Miguel Garoz <mgaroz@gmail.com>
2023-03-21 01:25:53 -0600

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script>
+	import '../app.css';
+</script>
+
+<header>
+	<nav class="flex items-center justify-between py-5 w-full px-20 h-32">
+		<div class="logo">LOGO</div>
+		<div class="menu">
+			<ul class="list-none flex">
+				<li class="inline-block py-5 px-[30px]">
+					<a href="/" class="text-[14px] uppercase ">Home</a>
+				</li>
+				<li class="inline-block py-5 px-[30px]">
+					<a href="#work" class="text-[14px] uppercase ">Work</a>
+				</li>
+				<li class="inline-block py-5 px-[30px]">
+					<a href="#about" class="text-[14px] uppercase ">About</a>
+				</li>
+				<li class="inline-block py-5 px-[30px]">
+					<a href="#contact" class="text-[14px] uppercase ">Contact</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
+</header>
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<section class="px-20">
+	<div id="hero" class="w-full relative z-20 overflow-hidden">
+		<div class="relative w-full inset-0 block mx-auto z-2">
+			<div class="py-40 w-full mx-auto relative text-left">
+				<h1 class="text-hero leading-none">
+					<span class="block relative">MIGUEL</span>
+					<span class="block relative left-96">GAROZ</span>
+				</h1>
+			</div>
+			<div class="absolute bottom-0 left-0 max-w-[16.25rem]" id="herosub-wrapper">
+				<h5 class="translate-y-[calc((-14.5vw+50%))] uppercase leading-5">
+					<span class="text-sm relative block"
+						>Front-end developer with an adaptive approach to problem solving.</span
+					>
+				</h5>
+			</div>
+		</div>
+		<div class="uppercase flex items-center justify-between w-full" id="hero-footer">
+			<div id="hero-footer-left">
+				<span class="inline-block text-sm">Scroll to explore</span>
+			</div>
+			<div id="hero-footer-right">
+				<span class="text-sm"> Selected case studies (04)</span>
+			</div>
+		</div>
+	</div>
+</section>
