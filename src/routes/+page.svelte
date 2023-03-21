3d60fa795eea49a10cf1f8d0c946e118c6fc7579
Miguel Garoz <mgaroz@gmail.com>
2023-03-21 01:43:04 -0600

    hero section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,14 @@
 <section class="px-20">
 	<div id="hero" class="w-full relative z-20 overflow-hidden">
 		<div class="relative w-full inset-0 block mx-auto z-2">
-			<div class="py-40 w-full mx-auto relative text-left">
+			<div class="pt-10 pb-40 w-full mx-auto relative text-left">
 				<h1 class="text-hero leading-none">
 					<span class="block relative">MIGUEL</span>
 					<span class="block relative left-96">GAROZ</span>
 				</h1>
 			</div>
 			<div class="absolute bottom-0 left-0 max-w-[16.25rem]" id="herosub-wrapper">
-				<h5 class="translate-y-[calc((-14.5vw+50%))] uppercase leading-5">
+				<h5 class="translate-y-[-20vh] translate-x-2 uppercase leading-5">
 					<span class="text-sm relative block"
 						>Front-end developer with an adaptive approach to problem solving.</span
 					>
