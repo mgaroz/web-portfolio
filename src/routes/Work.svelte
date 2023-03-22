e6a5a5b5149cdb3bf1dc5946fd80526eddd713ad
Miguel Garoz <mgaroz@gmail.com>
2023-03-22 02:09:13 -0600

    animated flex grow

--- src/routes/Work.svelte
+++ src/routes/Work.svelte
@@ -1,18 +1,30 @@
 <section class="h-full w-full px-20 pb-28" id="work">
 	<div id="work-section" class="flex items-center justify-between gap-8">
-		<div id="work-item-one" class="w-1/4 xl:h-[45vh] 2xl:h-[60vh]">
+		<div
+			id="work-item-one"
+			class="relative w-1/4 grow basis-0 transition-all duration-500 hover:grow-[2.5] xl:h-[45vh] 2xl:h-[60vh]"
+		>
 			<div class="bg-cod-gray-50 mb-4 h-full w-full" />
 			<p class="uppercase">Item One</p>
 		</div>
-		<div id="work-item-two" class="w-1/4 xl:h-[45vh] 2xl:h-[60vh]">
+		<div
+			id="work-item-two"
+			class="relative w-1/4 grow basis-0 transition-all duration-500 hover:grow-[2.5] xl:h-[45vh] 2xl:h-[60vh]"
+		>
 			<div class="bg-cod-gray-50 mb-4 h-full w-full" />
 			<p class="uppercase">Item Two</p>
 		</div>
-		<div id="work-item-three" class="w-1/4 xl:h-[45vh] 2xl:h-[60vh]">
+		<div
+			id="work-item-three"
+			class="relative w-1/4 grow basis-0 transition-all duration-500 hover:grow-[2.5] xl:h-[45vh] 2xl:h-[60vh]"
+		>
 			<div class="bg-cod-gray-50 mb-4 h-full w-full" />
 			<p class="uppercase">Item Three</p>
 		</div>
-		<div id="work-item-four" class="w-1/4 xl:h-[45vh] 2xl:h-[60vh]">
+		<div
+			id="work-item-four"
+			class="relative w-1/4 grow basis-0 transition-all duration-500 hover:grow-[2.5] xl:h-[45vh] 2xl:h-[60vh]"
+		>
 			<div class="bg-cod-gray-50 mb-4 h-full w-full" />
 			<p class="uppercase">Item Four</p>
 		</div>
