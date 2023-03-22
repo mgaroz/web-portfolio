4fc3d79c94172cc05996c7a556becdfee954b7ad
Miguel Garoz <mgaroz@gmail.com>
2023-03-22 03:13:32 -0600

    initial contact form

--- src/routes/Contact.svelte
+++ src/routes/Contact.svelte
@@ -1,8 +1,51 @@
 <!-- Needs contact form -->
 <section id="contact" class="h-full w-full px-20 pt-28">
+	<div class="pb-10">
+		<span class="text-big-size block uppercase leading-none">Get</span>
+		<span class="text-big-size block uppercase leading-none">In touch</span>
+	</div>
 	<div class="pb-20">
 		<span class="block text-[1.25rem] uppercase"> I'd love to hear from you </span>
 		<span class="block text-[1.25rem] uppercase"> Let's work together </span>
+	</div>
+	<div class="flex w-full items-center justify-between pb-28">
+		<div id="contact-left" class="w-1/4">
+			<h3 class="uppercase">Let's talk</h3>
+		</div>
+		<div id="contact-right" class="w-2/3">
+			<form action="/contact">
+				<div class="mb-8 grid w-full grid-cols-2 gap-10">
+					<div class="inline-block">
+						<label for="name" class="text-[20px]">NAME</label>
+						<input
+							type="text"
+							name="name"
+							id="name"
+							class="inline-block w-full border-b bg-transparent text-[20px]"
+						/>
+					</div>
+					<div class="inline-block">
+						<label for="email" class="text-[20px]">EMAIL</label>
+						<input
+							type="email"
+							name="email"
+							id="email"
+							class="inline-block w-full border-b bg-transparent text-[20px] outline-transparent"
+						/>
+					</div>
+				</div>
+				<div class="w-full">
+					<label for="message" class="text-[20px]">MESSAGE</label>
+					<textarea
+						name="message"
+						id="message"
+						cols="30"
+						rows="10"
+						class="w-full resize-none bg-transparent text-[20px]"
+					/>
+				</div>
+			</form>
+		</div>
 	</div>
 	<div class="flex items-center justify-between gap-8 pb-12">
 		<div class="flex items-center justify-between gap-8">
@@ -30,3 +73,9 @@
 		</div>
 	</div>
 </section>
+
+<style>
+	.text-big-size {
+		font-size: clamp(2.125rem, 1.2354rem + 3.9538vw, 5.98rem);
+	}
+</style>
