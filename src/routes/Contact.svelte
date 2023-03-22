80d8a2b2cbfaa3eeff376ed4629a651a0b7c14b6
Miguel Garoz <mgaroz@gmail.com>
2023-03-22 01:19:14 -0600

    added back to top link

--- src/routes/Contact.svelte
+++ src/routes/Contact.svelte
@@ -1,3 +1,31 @@
 <section id="contact" class="h-full w-full px-20 pt-28">
-	<div>CONTACT</div>
+	<div class="pb-20">
+		<span class="block text-[1.25rem] uppercase"> I'd love to hear from you </span>
+		<span class="block text-[1.25rem] uppercase"> Let's work together </span>
+	</div>
+	<div class="flex items-center justify-between gap-8 pb-12">
+		<div class="flex items-center justify-between gap-8">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="h-6 w-6"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25"
+				/>
+			</svg>
+			<span class="block text-[14px] uppercase">Back to top</span>
+		</div>
+		<div>
+			<span class="text-[14px] uppercase">Miguel Garoz &#169; 2023</span>
+		</div>
+		<div>
+			<span class="text-[14px] uppercase">Follow me</span>
+		</div>
+	</div>
 </section>
