5d106084b1a029855c82b9b147f36486ee105f7f
Miguel Garoz <mgaroz@gmail.com>
2023-03-22 22:34:15 -0600

    work right text opacity

--- src/routes/Work.svelte
+++ src/routes/Work.svelte
@@ -2,47 +2,75 @@
 	<div id="work-section" class="flex items-center justify-between gap-8">
 		<div
 			id="work-item-one"
-			class="relative w-1/4 grow basis-0 cursor-pointer transition-all duration-500 hover:grow-[2.5] xl:h-[45vh] 2xl:h-[60vh]"
+			class="group relative w-1/4 grow basis-0 cursor-pointer transition-all duration-500 hover:grow-[2.5] xl:h-[45vh] 2xl:h-[60vh]"
 		>
 			<div class="mb-4 h-full w-full overflow-hidden">
 				<div
 					class="bg-cod-gray-50 mb-4 h-full w-full scale-110 bg-[url('https://picsum.photos/id/236/800/600')] bg-cover bg-center bg-no-repeat transition-transform duration-500 hover:scale-100"
 				/>
 			</div>
-			<p class="uppercase">Item One</p>
+			<div class="flex items-center justify-between">
+				<p class="uppercase">Item One</p>
+				<p
+					class="text-gallery-800 uppercase opacity-0 transition-opacity duration-500 group-hover:opacity-100"
+				>
+					Development
+				</p>
+			</div>
 		</div>
 		<div
 			id="work-item-two"
-			class="relative w-1/4 grow basis-0 cursor-pointer transition-all duration-500 hover:grow-[2.5] xl:h-[45vh] 2xl:h-[60vh]"
+			class="group relative w-1/4 grow basis-0 cursor-pointer transition-all duration-500 hover:grow-[2.5] xl:h-[45vh] 2xl:h-[60vh]"
 		>
 			<div class="mb-4 h-full w-full overflow-hidden">
 				<div
 					class="bg-cod-gray-50 mb-4 h-full w-full scale-110 bg-[url('https://picsum.photos/id/235/800/600')] bg-cover bg-center bg-no-repeat transition-transform duration-500 hover:scale-100"
 				/>
 			</div>
-			<p class="uppercase">Item Two</p>
+			<div class="flex items-center justify-between">
+				<p class="uppercase">Item Two</p>
+				<p
+					class="text-gallery-800 uppercase opacity-0 transition-opacity duration-500 group-hover:opacity-100"
+				>
+					Development
+				</p>
+			</div>
 		</div>
 		<div
 			id="work-item-three"
-			class="relative w-1/4 grow basis-0 cursor-pointer transition-all duration-500 hover:grow-[2.5] xl:h-[45vh] 2xl:h-[60vh]"
+			class="group relative w-1/4 grow basis-0 cursor-pointer transition-all duration-500 hover:grow-[2.5] xl:h-[45vh] 2xl:h-[60vh]"
 		>
 			<div class="mb-4 h-full w-full overflow-hidden">
 				<div
 					class="bg-cod-gray-50 mb-4 h-full w-full scale-110 bg-[url('https://picsum.photos/id/234/800/600')] bg-cover bg-center bg-no-repeat transition-transform duration-500 hover:scale-100"
 				/>
 			</div>
-			<p class="uppercase">Item Three</p>
+			<div class="flex items-center justify-between">
+				<p class="uppercase">Item Three</p>
+				<p
+					class="text-gallery-800 uppercase opacity-0 transition-opacity duration-500 group-hover:opacity-100"
+				>
+					Development
+				</p>
+			</div>
 		</div>
 		<div
 			id="work-item-four"
-			class="relative w-1/4 grow basis-0 cursor-pointer transition-all duration-500 hover:grow-[2.5] xl:h-[45vh] 2xl:h-[60vh]"
+			class="group relative w-1/4 grow basis-0 cursor-pointer transition-all duration-500 hover:grow-[2.5] xl:h-[45vh] 2xl:h-[60vh]"
 		>
 			<div class="mb-4 h-full w-full overflow-hidden">
 				<div
 					class="bg-cod-gray-50 mb-4 h-full w-full scale-110 bg-[url('https://picsum.photos/id/233/800/600')] bg-cover bg-center bg-no-repeat transition-transform duration-500 hover:scale-100"
 				/>
 			</div>
-			<p class="uppercase">Item Four</p>
+			<div class="flex items-center justify-between">
+				<p class="uppercase">Item Four</p>
+				<p
+					class="text-gallery-800 uppercase opacity-0 transition-opacity duration-500 group-hover:opacity-100"
+				>
+					Development
+				</p>
+			</div>
 		</div>
 	</div>
 </section>
