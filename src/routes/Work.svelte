8747dbcbbfd1fb13fbc3cd7464547b951be87a66
Miguel Garoz <mgaroz@gmail.com>
2023-03-24 20:09:03 -0600

    average color from images

--- src/routes/Work.svelte
+++ src/routes/Work.svelte
@@ -1,10 +1,30 @@
 <script lang="ts">
+	import { FastAverageColor } from 'fast-average-color';
 	import { workActive, backColor } from '$lib/store';
+	import item1 from '$lib/img/sky-full.webp';
+	import item2 from '$lib/img/cf-landing.webp';
+	import item3 from '$lib/img/item3.avif';
+	import item4 from '$lib/img/item4.avif';
 
 	function setStatus(status: boolean, color: string) {
 		workActive.set(status);
 		backColor.set(color);
 	}
+
+	function newSetStatus(status: boolean, image: string) {
+		workActive.set(status);
+		fac
+			.getColorAsync(image)
+			.then((color) => {
+				backColor.set(color.hex);
+			})
+			.catch((e) => {
+				console.error(e);
+			});
+		fac.destroy();
+	}
+
+	const fac = new FastAverageColor();
 </script>
 
 <section class="h-full w-full px-20 pb-28" id="work">
@@ -12,7 +32,7 @@
 		<div
 			id="work-item-one"
 			class="group relative w-1/4 grow basis-0 cursor-pointer transition-all duration-500 hover:grow-[2.5] xl:h-[45vh] 2xl:h-[60vh]"
-			on:mouseenter={() => setStatus(true, '#9D7B6C')}
+			on:mouseenter={() => newSetStatus(true, item1)}
 			on:mouseleave={() => workActive.set(false)}
 		>
 			<div class="mb-4 h-full w-full overflow-hidden">
@@ -32,7 +52,7 @@
 		<div
 			id="work-item-two"
 			class="group relative w-1/4 grow basis-0 cursor-pointer transition-all duration-500 hover:grow-[2.5] xl:h-[45vh] 2xl:h-[60vh]"
-			on:mouseenter={() => setStatus(true, '#1F1C19')}
+			on:mouseenter={() => newSetStatus(true, item2)}
 			on:mouseleave={() => workActive.set(false)}
 		>
 			<div class="mb-4 h-full w-full overflow-hidden">
@@ -52,12 +72,12 @@
 		<div
 			id="work-item-three"
 			class="group relative w-1/4 grow basis-0 cursor-pointer transition-all duration-500 hover:grow-[2.5] xl:h-[45vh] 2xl:h-[60vh]"
-			on:mouseenter={() => setStatus(true, '#6A5C4D')}
+			on:mouseenter={() => newSetStatus(true, item3)}
 			on:mouseleave={() => workActive.set(false)}
 		>
 			<div class="mb-4 h-full w-full overflow-hidden">
 				<div
-					class="bg-cod-gray-50 mb-4 h-full w-full scale-110 bg-[url('https://picsum.photos/id/234/800/600')] bg-cover bg-center bg-no-repeat transition-transform duration-500 hover:scale-100"
+					class="bg-cod-gray-50 mb-4 h-full w-full scale-110 bg-[url('$lib/img/item3.avif')] bg-cover bg-center bg-no-repeat transition-transform duration-500 hover:scale-100"
 				/>
 			</div>
 			<div class="flex items-center justify-between">
@@ -72,12 +92,12 @@
 		<div
 			id="work-item-four"
 			class="group relative w-1/4 grow basis-0 cursor-pointer transition-all duration-500 hover:grow-[2.5] xl:h-[45vh] 2xl:h-[60vh]"
-			on:mouseenter={() => setStatus(true, '#3579AB')}
+			on:mouseenter={() => newSetStatus(true, item4)}
 			on:mouseleave={() => workActive.set(false)}
 		>
 			<div class="mb-4 h-full w-full overflow-hidden">
 				<div
-					class="bg-cod-gray-50 mb-4 h-full w-full scale-110 bg-[url('https://picsum.photos/id/233/800/600')] bg-cover bg-center bg-no-repeat transition-transform duration-500 hover:scale-100"
+					class="bg-cod-gray-50 mb-4 h-full w-full scale-110 bg-[url('$lib/img/item4.avif')] bg-cover bg-center bg-no-repeat transition-transform duration-500 hover:scale-100"
 				/>
 			</div>
 			<div class="flex items-center justify-between">
