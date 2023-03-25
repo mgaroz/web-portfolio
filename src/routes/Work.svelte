d22210bcac664d179dc9ad4194a4acc01652f5e9
Miguel Garoz <mgaroz@gmail.com>
2023-03-24 20:27:52 -0600

    replaced images with avif

--- src/routes/Work.svelte
+++ src/routes/Work.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import { FastAverageColor } from 'fast-average-color';
 	import { workActive, backColor } from '$lib/store';
-	import item1 from '$lib/img/sky-full.webp';
-	import item2 from '$lib/img/cf-landing.webp';
+	import item1 from '$lib/img/sky-full.avif';
+	import item2 from '$lib/img/cf-landing.avif';
 	import item3 from '$lib/img/item3.avif';
 	import item4 from '$lib/img/item4.avif';
 
-	function newSetStatus(status: boolean, image: string) {
+	function statusAndAverageColor(status: boolean, image: string) {
 		workActive.set(status);
 		fac
 			.getColorAsync(image)
@@ -27,12 +27,12 @@
 		<div
 			id="work-item-one"
 			class="group relative w-1/4 grow basis-0 cursor-pointer transition-all duration-500 hover:grow-[2.5] xl:h-[45vh] 2xl:h-[60vh]"
-			on:mouseenter={() => newSetStatus(true, item1)}
+			on:mouseenter={() => statusAndAverageColor(true, item1)}
 			on:mouseleave={() => workActive.set(false)}
 		>
 			<div class="mb-4 h-full w-full overflow-hidden">
 				<div
-					class="bg-cod-gray-50 mb-4 h-full w-full scale-110 bg-[url('$lib/img/sky-full.webp')] bg-cover bg-center bg-no-repeat transition-transform duration-500 group-hover:scale-100"
+					class="bg-cod-gray-50 mb-4 h-full w-full scale-110 bg-[url('$lib/img/sky-full.avif')] bg-cover bg-center bg-no-repeat transition-transform duration-500 group-hover:scale-100"
 				/>
 			</div>
 			<div class="flex items-center justify-between">
@@ -47,12 +47,12 @@
 		<div
 			id="work-item-two"
 			class="group relative w-1/4 grow basis-0 cursor-pointer transition-all duration-500 hover:grow-[2.5] xl:h-[45vh] 2xl:h-[60vh]"
-			on:mouseenter={() => newSetStatus(true, item2)}
+			on:mouseenter={() => statusAndAverageColor(true, item2)}
 			on:mouseleave={() => workActive.set(false)}
 		>
 			<div class="mb-4 h-full w-full overflow-hidden">
 				<div
-					class="bg-cod-gray-50 mb-4 h-full w-full scale-110 bg-[url('$lib/img/cf-landing.webp')] bg-cover bg-center bg-no-repeat transition-transform duration-500 hover:scale-100"
+					class="bg-cod-gray-50 mb-4 h-full w-full scale-110 bg-[url('$lib/img/cf-landing.avif')] bg-cover bg-center bg-no-repeat transition-transform duration-500 hover:scale-100"
 				/>
 			</div>
 			<div class="flex items-center justify-between">
@@ -67,7 +67,7 @@
 		<div
 			id="work-item-three"
 			class="group relative w-1/4 grow basis-0 cursor-pointer transition-all duration-500 hover:grow-[2.5] xl:h-[45vh] 2xl:h-[60vh]"
-			on:mouseenter={() => newSetStatus(true, item3)}
+			on:mouseenter={() => statusAndAverageColor(true, item3)}
 			on:mouseleave={() => workActive.set(false)}
 		>
 			<div class="mb-4 h-full w-full overflow-hidden">
@@ -87,7 +87,7 @@
 		<div
 			id="work-item-four"
 			class="group relative w-1/4 grow basis-0 cursor-pointer transition-all duration-500 hover:grow-[2.5] xl:h-[45vh] 2xl:h-[60vh]"
-			on:mouseenter={() => newSetStatus(true, item4)}
+			on:mouseenter={() => statusAndAverageColor(true, item4)}
 			on:mouseleave={() => workActive.set(false)}
 		>
 			<div class="mb-4 h-full w-full overflow-hidden">
