69f5f8c0fd5569437a25506d607aeb39de284aae
Miguel Garoz <mgaroz@gmail.com>
2023-03-26 02:53:12 -0600

    lower opacity of items not being hovered

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,17 @@
 	import Link from '$lib/components/Link.svelte';
 	import { active, activeNavItem } from '$lib/store';
 
+	let hoveredStatus = {
+		hovered: false,
+		item: ''
+	};
+
 	function handleEnter() {
 		active.set(true);
+	}
+
+	function itemHovered(status: boolean, item: string) {
+		hoveredStatus = { hovered: status, item: item };
 	}
 </script>
 
@@ -17,7 +26,12 @@
 				on:mouseenter={handleEnter}
 				on:mouseleave={() => active.set(false)}
 			>
-				<li class="group inline-block py-5 px-[30px]">
+				<li
+					class="group inline-block py-5 px-[30px]"
+					style="opacity: {hoveredStatus.item === 'home' ? 1 : hoveredStatus.hovered ? 0.5 : 1}"
+					on:mouseenter={() => itemHovered(true, 'home')}
+					on:mouseleave={() => itemHovered(false, '')}
+				>
 					<Link
 						href="/"
 						content="Home"
@@ -31,7 +45,12 @@
 						class="h-[1px] w-full border-t {$activeNavItem === 'home' ? 'visible' : 'invisible'}"
 					/>
 				</li>
-				<li class="group inline-block py-5 px-[30px]">
+				<li
+					class="group inline-block py-5 px-[30px]"
+					style="opacity: {hoveredStatus.item === 'work' ? 1 : hoveredStatus.hovered ? 0.5 : 1}"
+					on:mouseenter={() => itemHovered(true, 'work')}
+					on:mouseleave={() => itemHovered(false, '')}
+				>
 					<Link
 						href="#work"
 						content="Work"
@@ -44,7 +63,12 @@
 						class="h-[1px] w-full border-t {$activeNavItem === 'work' ? 'visible' : 'invisible'}"
 					/>
 				</li>
-				<li class="group inline-block py-5 px-[30px]">
+				<li
+					class="hovered group inline-block py-5 px-[30px]"
+					style="opacity: {hoveredStatus.item === 'about' ? 1 : hoveredStatus.hovered ? 0.5 : 1}"
+					on:mouseenter={() => itemHovered(true, 'about')}
+					on:mouseleave={() => itemHovered(false, '')}
+				>
 					<Link
 						href="#about"
 						content="About"
@@ -57,7 +81,12 @@
 						class="h-[1px] w-full border-t {$activeNavItem === 'about' ? 'visible' : 'invisible'}"
 					/>
 				</li>
-				<li class="group inline-block py-5 px-[30px]">
+				<li
+					class="hovered group inline-block py-5 px-[30px]"
+					style="opacity: {hoveredStatus.item === 'blog' ? 1 : hoveredStatus.hovered ? 0.5 : 1}"
+					on:mouseenter={() => itemHovered(true, 'blog')}
+					on:mouseleave={() => itemHovered(false, '')}
+				>
 					<Link
 						href="#blog"
 						content="Blog"
@@ -70,7 +99,12 @@
 						class="h-[1px] w-full border-t {$activeNavItem === 'blog' ? 'visible' : 'invisible'}"
 					/>
 				</li>
-				<li class="group my-5 ml-[30px] inline-block">
+				<li
+					class="group my-5 ml-[30px] inline-block"
+					style="opacity: {hoveredStatus.item === 'contact' ? 1 : hoveredStatus.hovered ? 0.5 : 1}"
+					on:mouseenter={() => itemHovered(true, 'contact')}
+					on:mouseleave={() => itemHovered(false, '')}
+				>
 					<Link
 						href="#contact"
 						content="Contact"
@@ -89,3 +123,9 @@
 </header>
 
 <slot />
+
+<style>
+	.hovered {
+		transition: opacity 0.2s;
+	}
+</style>
