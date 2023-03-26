07c7e6c5043ed7a257e2af8de7b196dbdaccf1f3
Miguel Garoz <mgaroz@gmail.com>
2023-03-26 01:00:19 -0600

    added store for active nav item

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -4,3 +4,4 @@ export const active = writable(false);
 export const workActive = writable(false);
 export const backColor = writable('#000');
 export const blogActive = writable(false);
+export const activeNavItem = writable('home');
