785caf8682fa8879d8c63283d25bcad866322274
Miguel Garoz <mgaroz@gmail.com>
2023-03-28 05:02:37 -0600

    social links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -121,40 +121,95 @@
 			class="2xs:pb-10 flex w-full items-center justify-between uppercase md:pb-0"
 			id="hero-footer"
 		>
-			<div
-				id="hero-footer-left"
-				class="font-bgr group flex items-center justify-center gap-8 overflow-hidden"
-			>
-				<div class="2xs:hidden md:block">
-					<Link
-						href="#portfolio"
-						content="Scroll to explore"
-						textSize="14px"
-						duration="200ms"
-						displacement="20px"
-					/>
-				</div>
-				<div class="flex h-5 w-5 items-center justify-center">
-					<a href="#portfolio">
+			<div class="flex flex-col">
+				<div class="flex items-center gap-4 md:pb-8">
+					<a href="https://github.com/mgaroz">
 						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
+							class="h-7 w-7 text-white"
+							width="24"
+							height="24"
 							viewBox="0 0 24 24"
-							stroke-width={2.5}
+							stroke-width="1"
 							stroke="currentColor"
-							class="h-5 w-5"
+							fill="none"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						>
+							<path stroke="none" d="M0 0h24v24H0z" />
+							<path
+								d="M9 19c-4.286 1.35-4.286-2.55-6-3m12 5v-3.5c0-1 .099-1.405-.5-2 2.791-.3 5.5-1.366 5.5-6.04a4.567 4.567 0 0 0 -1.333 -3.21 4.192 4.192 0 00-.08-3.227s-1.05-.3-3.476 1.267a12.334 12.334 0 0 0 -6.222 0C6.462 2.723 5.413 3.023 5.413 3.023a4.192 4.192 0 0 0 -.08 3.227A4.566 4.566 0 004 9.486c0 4.64 2.709 5.68 5.5 6.014-.591.589-.56 1.183-.5 2V21"
+							/></svg
+						>
+					</a>
+					<a href="https://www.linkedin.com/in/mgaroz/">
+						<svg
+							class="h-7 w-7 text-white"
+							viewBox="0 0 24 24"
+							fill="none"
+							stroke="currentColor"
+							stroke-width="1"
+							stroke-linecap="round"
+							stroke-linejoin="round"
 						>
 							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="M4.5 4.5l15 15m0 0V8.25m0 11.25H8.25"
-							/>
-						</svg>
+								d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"
+							/> <rect x="2" y="9" width="4" height="12" /> <circle cx="4" cy="4" r="2" /></svg
+						>
 					</a>
+					<a href="https://twitter.com/mgaroz_pro">
+						<svg
+							class="h-7 w-7 text-white"
+							width="24"
+							height="24"
+							viewBox="0 0 24 24"
+							stroke-width="1"
+							stroke="currentColor"
+							fill="none"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						>
+							<path stroke="none" d="M0 0h24v24H0z" />
+							<path
+								d="M22 4.01c-1 .49-1.98.689-3 .99-1.121-1.265-2.783-1.335-4.38-.737S11.977 6.323 12 8v1c-3.245.083-6.135-1.395-8-4 0 0-4.182 7.433 4 11-1.872 1.247-3.739 2.088-6 2 3.308 1.803 6.913 2.423 10.034 1.517 3.58-1.04 6.522-3.723 7.651-7.742a13.84 13.84 0 0 0 .497 -3.753C20.18 7.773 21.692 5.25 22 4.009z"
+							/></svg
+						>
+					</a>
+				</div>
+				<div
+					id="hero-footer-left"
+					class="font-bgr group flex items-center justify-center gap-8 overflow-hidden"
+				>
+					<div class="2xs:hidden md:block">
+						<Link
+							href="#portfolio"
+							content="Scroll to explore"
+							textSize="14px"
+							duration="200ms"
+							displacement="20px"
+						/>
+					</div>
+					<div class="flex h-5 w-5 items-center justify-center">
+						<a href="#portfolio">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width={2.5}
+								stroke="currentColor"
+								class="h-5 w-5"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M4.5 4.5l15 15m0 0V8.25m0 11.25H8.25"
+								/>
+							</svg>
+						</a>
+					</div>
 				</div>
 			</div>
 			<div id="hero-footer-right">
-				<span class="font-bgr text-sm">Selected works (04)</span>
+				<span class="font-bgr h-full text-sm">Selected works (04)</span>
 			</div>
 		</div>
 	</div>
