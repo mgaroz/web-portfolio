ddcc03af3c15efed7cf6be51bcd9519f34722f95
Miguel Garoz <mgaroz@gmail.com>
2023-03-31 01:48:05 -0600

    fix(app): portfolio is now grid @768px

--- src/routes/Work.svelte
+++ src/routes/Work.svelte
@@ -11,6 +11,7 @@
 	let colorObject: { [prop: string]: any } = {};
 	let portfolioItems = [
 		{
+			id: 'item1',
 			name: 'Skyler Cohen',
 			type: 'Development',
 			tech: 'Sveltekit, Tailwind',
@@ -19,6 +20,7 @@
 			img: item1
 		},
 		{
+			id: 'item2',
 			name: "Andy's Cafe",
 			type: 'Development',
 			tech: 'Sveltekit, Tailwind',
@@ -27,6 +29,7 @@
 			img: item2
 		},
 		{
+			id: 'item3',
 			name: 'Item 3',
 			type: 'Development',
 			tech: 'Sveltekit, Tailwind',
@@ -35,6 +38,7 @@
 			img: item3
 		},
 		{
+			id: 'item4',
 			name: 'Item 4',
 			type: 'Development',
 			tech: 'Sveltekit, Tailwind',
@@ -71,144 +75,91 @@
 <section class="2xs:px-6 h-full w-full pb-28 md:px-20" id="portfolio">
 	<div
 		id="work-section"
-		class="2xs:flex 2xs:justify-between 2xs:flex-col 2xs:gap-6 md:flex-row md:items-center md:gap-8"
+		class="2xs:flex 2xs:justify-between 2xs:flex-col 2xs:gap-6 md:grid md:grid-cols-2 md:gap-8 md:gap-y-16 xl:flex xl:flex-row xl:items-center xl:gap-8"
 	>
-		<div
-			id="work-item-one"
-			class="2xs:h-[50vmin] 2xs:flex 2xs:flex-col 2xs:w-full group relative transition-all duration-500 hover:grow-[2.5] md:block md:w-1/4 md:grow md:basis-0 xl:h-[45vh] 2xl:h-[60vh]"
-			on:mouseenter={() => setStatusAndColor(true, 'item1')}
-			on:mouseleave={() => workActive.set(false)}
-		>
-			<div class="relative mb-4 h-full w-full overflow-hidden">
-				<div
-					class="2xs:absolute 2xs:bottom-4 2xs:right-4 2xs:gap-4 2xs:z-20 flex"
-					on:mouseenter={() => workActive.set(false)}
-					on:mouseleave={() => setStatusAndColor(true, 'item1')}
-				>
-					<a
-						href="https://github.com/mgaroz"
-						target="_blank"
-						class="transition-opacity duration-500 md:block md:opacity-0 md:group-hover:opacity-100"
-						aria-label="Source"
-						><svg
-							class="h-7 w-7 text-white"
-							width="24"
-							height="24"
-							viewBox="0 0 24 24"
-							stroke-width="1"
-							stroke="currentColor"
-							fill="none"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-						>
-							<path stroke="none" d="M0 0h24v24H0z" />
-							<path
-								d="M9 19c-4.286 1.35-4.286-2.55-6-3m12 5v-3.5c0-1 .099-1.405-.5-2 2.791-.3 5.5-1.366 5.5-6.04a4.567 4.567 0 0 0 -1.333 -3.21 4.192 4.192 0 00-.08-3.227s-1.05-.3-3.476 1.267a12.334 12.334 0 0 0 -6.222 0C6.462 2.723 5.413 3.023 5.413 3.023a4.192 4.192 0 0 0 -.08 3.227A4.566 4.566 0 004 9.486c0 4.64 2.709 5.68 5.5 6.014-.591.589-.56 1.183-.5 2V21"
-							/></svg
-						></a
+		{#each portfolioItems as { id, name, type, tech, link, source, img }}
+			<div
+				class="2xs:h-[50vmin] 2xs:flex 2xs:flex-col 2xs:w-full group relative transition-all duration-500 hover:grow-[2.5] md:h-[25vh] xl:flex xl:h-[45vh] xl:w-1/4 xl:grow xl:basis-0 2xl:h-[60vh]"
+				on:mouseenter={() => setStatusAndColor(true, id)}
+				on:mouseleave={() => workActive.set(false)}
+			>
+				<div class="relative mb-4 min-h-full w-full overflow-hidden">
+					<div
+						class="font-bgr 2xs:absolute 2xs:left-0 2xs:h-[25%] sm: 2xs:bottom-0 2xs:w-full bg-cod-gray-900/60 z-20 flex items-center justify-between backdrop-blur-sm md:h-[10%] md:opacity-0 md:transition-opacity md:duration-500 md:group-hover:opacity-100"
 					>
-					<a
-						href="https://skylercohen.com.au"
-						target="_blank"
-						class="transition-opacity duration-500 md:block md:opacity-0 md:group-hover:opacity-100"
-						aria-label="Website"
-						><svg
-							class="h-7 w-7 text-white"
-							width="24"
-							height="24"
-							viewBox="0 0 24 24"
-							stroke-width="1"
-							stroke="currentColor"
-							fill="none"
-							stroke-linecap="round"
-							stroke-linejoin="round"
+						<p class="2xs:px-2 xs:px-4 text-[14px] uppercase">
+							{type}
+						</p>
+						<div
+							class="flex gap-4 pr-4"
+							on:mouseenter={() => workActive.set(false)}
+							on:mouseleave={() => setStatusAndColor(true, id)}
 						>
-							<path stroke="none" d="M0 0h24v24H0z" /> <circle cx="12" cy="12" r="9" />
-							<line x1="3.6" y1="9" x2="20.4" y2="9" />
-							<line x1="3.6" y1="15" x2="20.4" y2="15" /> <path d="M11.5 3a17 17 0 0 0 0 18" />
-							<path d="M12.5 3a17 17 0 0 1 0 18" /></svg
-						></a
-					>
+							<a
+								href={source}
+								target="_blank"
+								class="transition-opacity duration-500 md:block md:opacity-0 md:group-hover:opacity-100"
+								aria-label="Source"
+								><svg
+									class="h-7 w-7 text-white"
+									width="24"
+									height="24"
+									viewBox="0 0 24 24"
+									stroke-width="1"
+									stroke="currentColor"
+									fill="none"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								>
+									<path stroke="none" d="M0 0h24v24H0z" />
+									<path
+										d="M9 19c-4.286 1.35-4.286-2.55-6-3m12 5v-3.5c0-1 .099-1.405-.5-2 2.791-.3 5.5-1.366 5.5-6.04a4.567 4.567 0 0 0 -1.333 -3.21 4.192 4.192 0 00-.08-3.227s-1.05-.3-3.476 1.267a12.334 12.334 0 0 0 -6.222 0C6.462 2.723 5.413 3.023 5.413 3.023a4.192 4.192 0 0 0 -.08 3.227A4.566 4.566 0 004 9.486c0 4.64 2.709 5.68 5.5 6.014-.591.589-.56 1.183-.5 2V21"
+									/></svg
+								></a
+							>
+							<a
+								href={link}
+								target="_blank"
+								class="transition-opacity duration-500 md:block md:opacity-0 md:group-hover:opacity-100"
+								aria-label="Website"
+								><svg
+									class="h-7 w-7 text-white"
+									width="24"
+									height="24"
+									viewBox="0 0 24 24"
+									stroke-width="1"
+									stroke="currentColor"
+									fill="none"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								>
+									<path stroke="none" d="M0 0h24v24H0z" /> <circle cx="12" cy="12" r="9" />
+									<line x1="3.6" y1="9" x2="20.4" y2="9" />
+									<line x1="3.6" y1="15" x2="20.4" y2="15" /> <path d="M11.5 3a17 17 0 0 0 0 18" />
+									<path d="M12.5 3a17 17 0 0 1 0 18" /></svg
+								></a
+							>
+						</div>
+					</div>
+					<div
+						style="background-image: url('{img}')"
+						class="bg-cod-gray-50 relative mb-4 flex h-full w-full scale-110 flex-col items-end justify-end bg-cover bg-center bg-no-repeat text-white transition-transform duration-500 group-hover:scale-100"
+					/>
 				</div>
-				<div
-					style="background-image: url('{item1}')"
-					class="bg-cod-gray-50 relative mb-4 flex h-full w-full scale-110 flex-col items-end justify-end bg-cover bg-center bg-no-repeat text-white transition-transform duration-500 group-hover:scale-100"
-				/>
+				<div class="2xs:w-full flex max-h-full items-center justify-between">
+					<p
+						class="font-bgr p1 2xs:whitespace-normal 2xs:opacity-100 uppercase md:whitespace-nowrap"
+					>
+						{name}
+					</p>
+					<p
+						class="text-gallery-800 p2 2xs:hidden 2xs:whitespace-nowrap uppercase opacity-0 transition-all duration-500 group-hover:opacity-100 md:block"
+					>
+						{tech}
+					</p>
+				</div>
 			</div>
-			<div class="2xs:w-full flex items-center justify-between">
-				<p class="font-bgr p1 2xs:whitespace-normal 2xs:opacity-100 uppercase md:whitespace-nowrap">
-					Skyler Cohen
-				</p>
-				<p
-					class="text-gallery-800 p2 2xs:hidden uppercase opacity-0 transition-opacity duration-500 group-hover:opacity-100 md:block"
-				>
-					Development
-				</p>
-			</div>
-		</div>
-		<div
-			id="work-item-two"
-			class="2xs:h-[50vmin] 2xs:flex 2xs:flex-col 2xs:w-full group relative cursor-pointer transition-all duration-500 md:block md:w-1/4 md:grow md:basis-0 md:hover:grow-[2.5] xl:h-[45vh] 2xl:h-[60vh]"
-			on:mouseenter={() => setStatusAndColor(true, 'item2')}
-			on:mouseleave={() => workActive.set(false)}
-		>
-			<div class="mb-4 h-full w-full overflow-hidden">
-				<div
-					style="background-image: url('{item2}')"
-					class="bg-cod-gray-50 mb-4 h-full w-full scale-110 bg-cover bg-center bg-no-repeat transition-transform duration-500 hover:scale-100"
-				/>
-			</div>
-			<div class="flex items-center justify-between">
-				<p class="p1 font-bgr uppercase">Andy's Cafe</p>
-				<p
-					class="text-gallery-800 p2 2xs:hidden uppercase opacity-0 transition-opacity duration-500 group-hover:opacity-100 md:block"
-				>
-					Development
-				</p>
-			</div>
-		</div>
-		<div
-			id="work-item-three"
-			class="2xs:h-[50vmin] 2xs:flex 2xs:flex-col 2xs:w-full group relative cursor-pointer transition-all duration-500 hover:grow-[2.5] md:block md:w-1/4 md:grow md:basis-0 xl:h-[45vh] 2xl:h-[60vh]"
-			on:mouseenter={() => setStatusAndColor(true, 'item3')}
-			on:mouseleave={() => workActive.set(false)}
-		>
-			<div class="mb-4 h-full w-full overflow-hidden">
-				<div
-					style="background-image: url('{item3}')"
-					class="bg-cod-gray-50 mb-4 h-full w-full scale-110 bg-cover bg-center bg-no-repeat transition-transform duration-500 hover:scale-100"
-				/>
-			</div>
-			<div class="flex items-center justify-between">
-				<p class="p1 font-bgr uppercase">Item Three</p>
-				<p
-					class="text-gallery-800 p2 2xs:hidden uppercase opacity-0 transition-opacity duration-500 group-hover:opacity-100 md:block"
-				>
-					Development
-				</p>
-			</div>
-		</div>
-		<div
-			id="work-item-four"
-			class="2xs:w-full 2xs:flex 2xs:flex-col 2xs:h-[50vmin] group relative cursor-pointer transition-all duration-500 hover:grow-[2.5] md:block md:w-1/4 md:grow md:basis-0 xl:h-[45vh] 2xl:h-[60vh]"
-			on:mouseenter={() => setStatusAndColor(true, 'item4')}
-			on:mouseleave={() => workActive.set(false)}
-		>
-			<div class="mb-4 h-full w-full overflow-hidden">
-				<div
-					style="background-image: url('{item4}')"
-					class="bg-cod-gray-50 mb-4 h-full w-full scale-110 bg-cover bg-center bg-no-repeat transition-transform duration-500 hover:scale-100"
-				/>
-			</div>
-			<div class="flex items-center justify-between">
-				<p class="p1 font-bgr uppercase">Item Four</p>
-				<p
-					class="text-gallery-800 p2 2xs:hidden uppercase opacity-0 transition-opacity duration-500 group-hover:opacity-100 md:block"
-				>
-					Development
-				</p>
-			</div>
-		</div>
+		{/each}
 	</div>
 </section>
 
