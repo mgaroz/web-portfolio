854f5695c5a7408f49bf0e91b9afd68df6401622
Miguel Garoz <mgaroz@gmail.com>
2023-04-05 23:17:52 -0600

    removed unused code

--- src/lib/components/Link.svelte
+++ src/lib/components/Link.svelte
@@ -2,10 +2,6 @@
 	import { activeMenuInfo, active } from '$lib/store';
 	export let href: string;
 	export let content: string | number;
-	export let textSize: string;
-	export let duration: string;
-	export let displacement: string = '19px';
-	// export let menuItem: boolean = false;
 
 	function setItemBeingHovered(item: string) {
 		$activeMenuInfo.itemBeingHovered = item;
@@ -25,7 +21,6 @@
 	}
 </script>
 
-<!-- {#if menuItem} -->
 <div class="flex h-full w-full flex-col items-center justify-center">
 	<a
 		{href}
@@ -40,75 +35,10 @@
 		>
 	</a>
 	<div
-		class="h-[1px] w-full border-t group-hover:visible {!$active &&
+		class="mt-[2px] h-[1px] w-full border-t group-hover:visible {!$active &&
 		!$activeMenuInfo.isBeingHovered &&
 		$activeMenuInfo.currentActiveItem === content.toString().toLowerCase()
 			? 'visible'
 			: 'invisible'}"
 	/>
 </div>
-
-<!-- {:else}
-	<a {href} class="block overflow-hidden uppercase">
-		<span
-			class="slide-up transition-[top] group-hover:-top-[var(--displacement)]"
-			data-hover={content}
-			style="--textSize:{textSize}; --duration:{duration}; --displacement:{displacement}"
-			>{content}</span
-		>
-	</a>
-{/if} -->
-<style>
-	.slide-up {
-		position: relative;
-		top: 0px;
-		width: auto;
-	}
-
-	.slide-up::before {
-		position: absolute;
-		width: auto;
-		top: 105%;
-		left: 0;
-		content: attr(data-hover);
-	}
-
-	.slide-up::after {
-		clear: both;
-		content: ' ';
-		display: block;
-		height: 0;
-		visibility: hidden;
-	}
-
-	/* .clear-text {
-		position: relative;
-		padding: 0;
-		overflow: hidden;
-		transition: transform 0.15s;
-		display: block;
-	} */
-
-	/* .clear-text::after {
-		content: attr(data-hover);
-		display: inline-block;
-		transform: translateY(100%);
-		position: absolute;
-		left: 0;
-		transition: inherit;
-	} */
-
-	/* .clear-text > span {
-		display: inline-block;
-		transform: translateY(0%);
-		transition: inherit;
-	} */
-
-	/* .clear-text:hover > span {
-		transform: translateY(-100%);
-	} */
-
-	/* .clear-text:hover::after {
-		transform: translateY(0%);
-	} */
-</style>
