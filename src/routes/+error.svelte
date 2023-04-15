72ef29a10d7d6798498f1bba7851e990d4318f9c
Miguel Garoz <mgaroz@gmail.com>
2023-04-15 01:31:19 -0600

    error padding

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,7 +2,7 @@
 	import { page } from '$app/stores';
 </script>
 
-<section id="error" class="error px-20 pt-32">
+<section id="error" class="error 2xs:px-5 pt-32 md:px-20">
 	<div class="flex h-full flex-col items-start justify-center">
 		<h1>{$page.status}:</h1>
 		<h2>{$page.error?.message}</h2>
