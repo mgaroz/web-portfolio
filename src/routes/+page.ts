5338d3d3e2c6dd01f89a649f1de55f816499b760
Miguel Garoz <mgaroz@umes.edu.gt>
2024-03-26 14:26:08 -0600

    changed load function

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,16 +1,7 @@
 export async function load() {
-	const response = () => {
-		try {
-			const res = fetch('https://dev.to/api/articles/latest?username=mgaroz&per_page=3').then(
-				(result) => result.json()
-			);
-			return res;
-		} catch (error) {
-			console.error(error);
-		}
-	};
+	const response = await fetch('https://dev.to/api/articles/latest?username=mgaroz&per_page=3');
 
 	return {
-		response: await response()
+		response: response
 	};
 }
