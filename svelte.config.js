91921af8c6fc7e43710f9701543808d7bd28d023
Miguel Garoz <mgaroz@umes.edu.gt>
2024-03-26 13:49:50 -0600

    removed routes

--- svelte.config.js
+++ svelte.config.js
@@ -12,10 +12,10 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			},
+			// routes: {
+			// 	include: ['/*'],
+			// 	exclude: ['<all>']
+			// },
 			pages: 'build',
 			assets: 'build',
 			fallback: undefined,
