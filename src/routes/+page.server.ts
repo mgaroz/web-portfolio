e10f354bf74b6ecacc36f3113b77d5ad284c0c60
Miguel Garoz <mgaroz@umes.edu.gt>
2024-03-26 15:42:42 -0600

    await response

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,6 +2,6 @@ export async function load() {
 	const response = await fetch('https://dev.to/api/articles/latest?username=mgaroz&per_page=3');
 
 	return {
-		response: response.json()
+		response: await response.json()
 	};
 }
