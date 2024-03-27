96f0a061202157b1bdffaaf1b39b2644c8e70b73
Miguel Garoz <mgaroz@umes.edu.gt>
2024-03-27 00:08:30 -0600

    changed load function type

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,6 +1,6 @@
-import type { Load } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
 
-export const load: Load = async () => {
+export const load: PageLoad = async () => {
 	console.log('fetching posts...');
 	const response = await fetch('https://dev.to/api/articles/latest?username=mgaroz&per_page=3');
 	const resp = await response.json();
