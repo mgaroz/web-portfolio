91b847218e6b76406f742ae6977f72c8245a44d5
Miguel Garoz <mgaroz@umes.edu.gt>
2024-03-26 22:33:30 -0600

    refactored load function

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,14 @@
-export async function load() {
-	const response = await fetch('https://dev.to/api/articles/latest?username=mgaroz&per_page=3');
+import type { PageServerLoad } from './$types';
 
+export const load: PageServerLoad = async () => {
+	console.log('fetching posts...');
+	const response = await fetch('https://dev.to/api/articles/latest?username=mgaroz&per_page=3');
+	const resp = await response.json();
+
+	if (!response.ok) {
+		throw new Error(response.statusText);
+	}
 	return {
-		response: await response.json()
+		response: resp
 	};
-}
+};
