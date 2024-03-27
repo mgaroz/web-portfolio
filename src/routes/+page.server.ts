7dfcdd4d6a153011ae4c54df4f5067a9e2dcac24
Miguel Garoz <mgaroz@umes.edu.gt>
2024-03-27 17:45:16 -0600

    removed unused var

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,13 +1,11 @@
-import { fail } from '@sveltejs/kit';
 import { _sendEmail } from './+server';
 
 export const actions = {
 	default: async ({ request }) => {
 		const form = Object.fromEntries(await request.formData());
-		let response;
 
 		if (Object.keys(form).length > 0) {
-			response = await _sendEmail(form?.name, form?.email, form?.message);
+			await _sendEmail(form?.name, form?.email, form?.message);
 		}
 	}
 };
