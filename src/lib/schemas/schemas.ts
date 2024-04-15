a4f106f1923b0c8eff0b3e9a01bfca7622e6a4f4
Miguel Garoz <mgaroz@gmail.com>
2024-04-14 18:51:50 -0600

    feat(tools): added error messages
    validation

--- src/lib/schemas/schemas.ts
+++ src/lib/schemas/schemas.ts
@@ -2,6 +2,9 @@ import { z } from 'zod';
 
 export const newContactSchema = z.object({
 	name: z.string({ required_error: 'Name is required' }).min(1),
-	email: z.string({ required_error: 'Email is required' }).email(),
-	message: z.string({ required_error: 'A message is required' }).min(10)
+	email: z.string({ required_error: 'Email is required' }).email('Check your email address'),
+	message: z
+		.string({ required_error: 'A message is required' })
+		.min(10)
+		.max(1024, "That's a long message, try getting to the point quicker!")
 });
