f35324b039259519180ac3f55e59ad1d116e6cbf
Miguel Garoz <mgaroz@gmail.com>
2024-05-31 04:27:02 -0600

    removed headers

--- src/hooks.server.ts
+++
@@ -1,8 +0,0 @@
-export const handle = async ({ event, resolve }) => {
-	const response = await resolve(event);
-
-	response.headers.set('Cross-Origin-Embedder-Policy', 'credentialless');
-	response.headers.set('Cross-Origin-Opener-Policy', 'same-origin');
-
-	return response;
-};
