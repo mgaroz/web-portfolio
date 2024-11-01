51fab36c693b8e7ae03031d31cccc98578784963
Miguel Garoz <mgaroz@gmail.com>
2024-10-31 20:36:22 -0600

    fix(app): added back social icons

--- src/lib/components/SocialIcons.svelte
+++ src/lib/components/SocialIcons.svelte
@@ -2,10 +2,7 @@
 	export let socialIconsContainer: HTMLDivElement;
 </script>
 
-<div
-	class="2xs:pb-4 flex w-full -translate-x-40 items-center gap-4 md:pb-8"
-	bind:this={socialIconsContainer}
->
+<div class="2xs:pb-4 flex w-full items-center gap-4 md:pb-8" bind:this={socialIconsContainer}>
 	<a href="https://github.com/mgaroz" target="_blank" aria-label="Github" class="group">
 		<svg
 			class="h-7 w-7 transition-transform duration-200 lg:group-hover:scale-125 dark:text-white"
