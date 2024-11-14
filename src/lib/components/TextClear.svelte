bdf784ad06d9f353afd828a42235489a39ae9a09
Miguel Garoz <mgaroz@gmail.com>
2024-11-14 11:09:11 -0600

    refactor(app): removed unused code

--- src/lib/components/TextClear.svelte
+++ src/lib/components/TextClear.svelte
@@ -6,47 +6,15 @@
 		rel?: string;
 	}
 
-	let {
-		text,
-		href = '/',
-		target = '_self',
-		rel = 'noreferrer'
-	}: Props = $props();
+	let { text, href = '/', target = '_self', rel = 'noreferrer' }: Props = $props();
 </script>
 
-<a {href} {target} {rel} data-after={text} class="text--effect-clear font-bgr text-[14px] uppercase">
+<a
+	{href}
+	{target}
+	{rel}
+	data-after={text}
+	class="text--effect-clear font-bgr text-[14px] uppercase"
+>
 	<span>{text}</span>
 </a>
-
-<!-- <style>
-	.clear-text {
-		position: relative;
-		padding: 0;
-		overflow: hidden;
-		transition: transform 0.2s;
-		display: block;
-	}
-
-	.clear-text::after {
-		content: attr(data-after);
-		display: inline-block;
-		transform: translateY(100%);
-		position: absolute;
-		left: 0;
-		transition: inherit;
-	}
-
-	.clear-text > span {
-		display: inline-block;
-		transform: translateY(0%);
-		transition: inherit;
-	}
-
-	.clear-text:hover > span {
-		transform: translateY(-100%);
-	}
-
-	.clear-text:hover::after {
-		transform: translateY(0%);
-	}
-</style> -->
